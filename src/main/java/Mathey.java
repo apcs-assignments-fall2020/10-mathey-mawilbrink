3f864eef4267325f4d1e0208fad72f6522e9e827
mawilbrink <martawilbrink@gmail.com>
2020-10-15 18:29:22 +0000

    in-class assignments 10/15

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,89 @@
 public class Mathey {
 
+
+    public static int max(int x, int y){
+        if(x > y){
+            return x;
+        }
+        else{
+            return y;
+        }
+    }
+    public static double max(double x, double y){
+        if(x > y){
+            return x;
+        }
+        else{
+            return y;
+        }
+
+    }
+    public static int max(int x, int y, int z){
+        if (x > y && x > z){
+            return x;
+        }
+        if( y > x && y > z){
+            return y;
+        }
+        else{
+            return z;
+        }
+    }
+    public static double max(double x, double y, double z){
+        if (x > y && x > z){
+            return x;
+        }
+        if( y > x && y > z){
+            return y;
+        }
+        else{
+            return z;
+        }
+    }
+    public static int min(int x, int y){
+        if(x > y){
+            return y;
+        }
+        else{
+            return x;
+        }
+    }
+    public static double min(double x, double y){
+        if(x > y){
+            return y;
+        }
+        else{
+            return x;
+        }
+
+    }
+    public static int min(int x, int y, int z){
+        if (x < y && x < z){
+            return x;
+        }
+        if( y < x && y < z){
+            return y;
+        }
+        else{
+            return z;
+        }
+    }
+    public static double min(double x, double y, double z){
+        if (x < y && x < z){
+            return x;
+        }
+        if( y < x && y < z){
+            return y;
+        }
+        else{
+            return z;
+        }
+    }
+    public static int randomInteger(int a, int b) { 
+        return (int) Math.random()*(b-a+1)+a;
+    }
+    
+    public static int randomInteger(int b) { 
+        return (int) Math.random()*(b+1);
+    }
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,11 +3,36 @@ import java.util.Scanner;
 public class MyMain {
 
     public static int someMethod() {
-        return 1;
+        double randInt = Math.random()*(11.0);
+        int randInt2 = (int) randInt+10;
+    
+        return randInt2;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int int1 = someMethod();
+        int int2 = someMethod();
+        int int3 = someMethod();
+
+        System.out.println("Three random numbers are "+int1+ ", "+int2+" and "+int3+".");
+
+        for(int i = 0; i < 2; i++){
+            if (int1 > int2){
+                int placeholder1 = int1;
+                int1 = int2;
+                int2 = placeholder1;
+            }
+            if (int2 > int3){
+                int placeholder2 = int2;
+                int2 = int3;
+                int3 = placeholder2;
+            }
+        }
+
+        System.out.println("The largest number is "+int3);        
+        System.out.println("The smallest number is "+int1);  
+
+        
     }
 
 }
